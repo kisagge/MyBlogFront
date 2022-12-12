ad978cbb69c5d7089cc65bc48218b7e3258ee20c
kisagge <kisagge@naver.com>
2022-12-12 18:46:11 +0900

    feat: 회원가입 api 연결

--- src/api/schema.d.ts
+++ src/api/schema.d.ts
@@ -1,3 +1,9 @@
+export interface ResType<T> {
+  data: T | null;
+  error: string | null;
+  result: boolean;
+}
+
 export interface LoginReq {
   userId: string;
   password: string;
@@ -5,4 +11,9 @@ export interface LoginReq {
 
 export interface LoginRes {
   token: string;
+}
+
+export interface SignUpReq {
+  userId: string;
+  password: string;
 }
